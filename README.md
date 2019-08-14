f8d2ac3450af3972f9dcaaab1f55841521ef41ce
wangzhefeng <wangzhefengr@163.com>
2019-08-14 23:28:19 +0800

    feature trans

--- README.md
+++ README.md
@@ -16,20 +16,26 @@
 		- 2.5 特征缩放【尺度】
 			- 2.5.1 Min-Max 特征缩放
 			- 2.5.2 标准化\(方差缩放\)
-			- 2.5.3 正规化
+			- 2.5.3 L1/L2 归一化
+			- 2.5.4 稳健缩放
 	- 3.Feature Building
 		- 3.1 简单的特征组合
-		- 3.1 多项式特征
-		- 3.2
+		- 3.2 多项式特征
 	- 4.Feature Selection
+		- 4.1 过滤
+		- 4.2 打包
+		- 4.3 嵌入式
 	- 5.Imbalanced Sample
+		- 5.1 过拟合
+		- 5.2 欠拟合
+		- 5.3 SMOTE
 
 <!-- /MarkdownTOC -->
 
 
 # Feature Engine
 
-* 数值型特征
+* **数值型特征**
 	- 特征合理性检查
 		+ 量级
 		+ 正负
@@ -48,8 +54,8 @@
 		+ 交互特征
 	- 特征选择
 		+ 
-* 类别型特征
-* 分类任务目标变量
+* **类别型特征**
+	- 分类任务目标变量
 
 ## 1.Feature Preprocessing
 
@@ -62,12 +68,17 @@
 
 ## 2.Feature Transform
 
-
-
 ### 2.1 计数特征二值化【尺度】
 
 * 当数据被大量且快速地生成时很有可能包含一些极端值，这时就应该检查数据的尺度，确定是应该保留数据的原始数值形式，还是应该将他们转换为二值数据，或者进行粗粒度的分箱操作；
 * 二值目标变量是一个既简单又稳健的衡量指标；
+
+
+```python
+from sklearn.preprocessing import Binarizer
+bined = Binarizer(threshod = 1, copy = True)
+transformed_data = bined.fit_transform(data)
+```
 
 
 ### 2.2 计数特征分箱(区间量化)【分布】
@@ -80,6 +91,14 @@
 * 为了对数据进行区间量化，必须确定每个分箱的宽度：
 	- 固定宽度分箱
 	- 自适应分箱
+
+```python
+from sklearn.preprocessing import KBinsDiscretizer
+kbins1 = KBinsDiscretizer(n_bins = 5, encode = "onehot", strategy = "quantile")
+kbins2 = KBinsDiscretizer(n_bins = 5, encode = "onehot-dense", strategy = "uniform")
+kbins3 = KBinsDiscretizer(n_bins = 5, encode = "ordinal", strategy = "kmeans")
+transformed_data = kbins1.fit_transform(data)
+```
 
 #### 2.2.1 固定宽度分箱
 
@@ -96,6 +115,7 @@ np.floor_divide(X, 10)
 ```python
 np.floor(np.log10(X))
 ```
+
 
 
 #### 2.2.2 分位数分箱
@@ -139,28 +159,52 @@ feature = feature.quantile([])
 
 #### 2.5.1 Min-Max 特征缩放
 
-* Min-Max 特征缩放可以将特征值压缩(或扩展)到$[0, 1]$区间中；
+> * Min-Max 特征缩放可以将特征值压缩(或扩展)到$[0, 1]$区间中；
 
 $$\frac{x - min(x)}{max(x) - min(x)}$$
 
 ```python
-
+from sklearn.preprocessing import MinMaxScaler
+mms = MinMaxScaler()
+featureScaled = mms.fit_transform(feature)
 ```
 
 
 #### 2.5.2 标准化(方差缩放)
 
-> 标准化后的特征均值为0，方差为1；
-> 如果初始特征服从正态分布，标准化后的特征也服从正态分布(标准正态分布)
+> * 标准化后的特征均值为0，方差为1；
+> * 如果初始特征服从正态分布，标准化后的特征也服从正态分布(标准正态分布);
+> * 不要中心化稀疏数据;
 
 $$\frac{x - mean(x)}{\sqrt(var(x))}$$
 
 ```python
-
+from sklearn.preprocessing import StandardScaler
+ss = StandardScaler()
+featureScaled = ss.fit_transform()(feature)
 ```
 
 
-#### 2.5.3 正规化
+#### 2.5.3 L1/L2 归一化
+
+> * 将特征除以一个归一化常数，比如：$l2$, $l1$ 范数，使得特征的范数为为常数；
+> * 归一化不会改变特征的分布；
+
+
+#### 2.5.4 稳健缩放
+
+> * 对于包含异常值的特征，标准化的效果不好，可以使用稳健的特征缩放技术对特征进行缩放；
+
+```python
+from sklearn.preprocessing import RobustScaler
+rs = RobustScaler(with_centering = True,
+				  with_scaling = True,
+				  quantile_range = (25.0, 75.0),
+				  copy = True)
+transform_data = rs.fit_transform(data)
+```
+
+
 
 
 ## 3.Feature Building
@@ -168,13 +212,46 @@ $$\frac{x - mean(x)}{\sqrt(var(x))}$$
 
 ### 3.1 简单的特征组合
 
-### 3.1 多项式特征
-
-### 3.2 
+### 3.2 多项式特征
 
 
 
 ## 4.Feature Selection
 
+> 特征选择技术可以精简掉无用的特征，以降低最终模型的复杂性，它的最终目的是得到一个简约模型，在不降低预测准确率或对预测准确率影响不大的情况下提高计算速度；
+
+### 4.1 过滤
+
+
+### 4.2 打包
+
+
+### 4.3 嵌入式
+
+> 将特征选择作为模型训练过程的一部分；
+
+* 特征选择是决策书与生俱来的功能，因为它在每个训练阶段都要选择一个特征来对树进行分割；
+	- 决策树
+	- GBM
+	- XGBoost
+	- LightGBM
+	- CatBoost
+	- RandomForest
+* $L1$ 正则化可以添加到任意线性模型的训练目标中，$L1$ 正则化鼓励模型使用更少的特征，所以也称为稀疏性约束；
+	- LASSO
+
+
+
 
 ## 5.Imbalanced Sample
+
+### 5.1 过拟合
+
+
+### 5.2 欠拟合
+
+
+### 5.3 SMOTE
+
+
+
--- featureTransform.py
+++ featureTransform.py
@@ -7,240 +7,276 @@
 
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import OrdinalEncoder
-from sklearn.preprocessing import OneHotEncoder
-import numpy as np
-from sklearn import preprocessing
-# from sklearn.preprocessing import KBinDiscretizer
-from sklearn.preprocessing import Binarizer
-from sklearn.preprocessing import binarize
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import Normalizer
-from sklearn.preprocessing import normalize
-import numpy as np
-import pandas as pd
-from sklearn import preprocessing
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import QuantileTransformer
-from sklearn.preprocessing import PowerTransformer
-import numpy as np
-import pandas as pd
-from scipy.stats import skew
+from sklearn import preprocessing
+
+# 标准化
+from sklearn.preprocessing import StandardScaler
+# from sklearn.preprocessing import scale
+
+# 特征缩放到一个范围
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MaxAbsScaler
+
+# 特征稳健缩放(存在异常值特征)
+from sklearn.preprocessing import RobustScaler
+from sklearn.preprocessing import robust_scale
+
+# 分箱离散化
+from sklearn.preprocessing import Binarizer
+# from sklearn.preprocessing import binarize
+from sklearn.preprocessing import KBinDiscretizer
+
+# 类别型特征重新编码
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import OneHotEncoder
+
+
+from sklearn.preprocessing import Normalizer
+from sklearn.preprocessing import normalize
+from sklearn.preprocessing import Quantiletransformer
+from sklearn.preprocessing import Powertransformer
 from sklearn.preprocessing import label_binarize
-from sklearn.compose import TransformedTargetRegressor
-from sklearn.preprocessing import QuantileTransformer
+from sklearn.compose import transformedTargetRegressor
+from scipy.stats import skew
 
 
 
-
-
-def oneHotEncoding(data, limit_value = 10):
-	"""
-	One-Hot Encoding: pandas get_dummies
-	:param data:
-	:param limit_value:
-	:return:
-	"""
-	feature_cnt = data.shape[1]
-	class_index = []
-	class_df = pd.DataFrame()
-	normal_index = []
-	for i in range(feature_cnt):
-		if len(pd.DataFrame(data.iloc[:, i]).drop_duplicates()) < limit_value:
-			class_index.append(i)
-			class_df = pd.concat([class_df, pd.get_dummies(data.iloc[:, i], prefix = data.columns[i])], axis = 1)
-		else:
-			normal_index.append(i)
-	data_update = pd.concat([data.iloc[:, normal_index], class_df], axis = 1)
-	return data_update
-
-
-def order_encoder(cate_feats):
-	enc = OrdinalEncoder()
-	encoded_feats = enc.fit_transform(cate_feats)
-
-	return encoded_feats
-
-
-def one_hot_encoder(cate_feats):
-	"""
-	One-Hot Encoding: sklearn.preprocessing.OneHotEncoder
-	:param cate_feats:
-	:return:
-	"""
-	enc = OneHotEncoder()
-	encoded_feats = enc.fit_transform(cate_feats)
-
-	return encoded_feats
-
-def k_bins(data, n_bins, encoder = "ordinal", strategy = "quantile"):
-	"""
-	分箱
-	:param data:
-	:param n_bins:
-	:param encoder:
-	:param strategy:
-	:return:
-	"""
-	est = preprocessing.KBinsDiscretizer(n_bins = n_bins,
-										 encoder = encoder,
-										 strategy = strategy)
-	est.fit_transform(data)
-
-
-def binarization(data, threshold = 0.0, is_copy = True):
-	"""
-	二值化
-	:param feat:
-	:param threshold:
-	:return:
-	"""
-	transformed_data = binarize(X = data, threshold = threshold, copy = is_copy)
-
-	return transformed_data
-
-
+# ================================================
+# 数值型变量分布转换
+# ================================================
 def standard_center(data, is_copy = True, with_mean = True, with_std = True):
-	"""
-	标准化,中心化
-	:return:
-	"""
-	ss = StandardScaler(copy = is_copy, with_mean = with_mean, with_std = with_std)
-	transformed_data = ss.fit_transform(data)
+    """
+    标准化/方差缩放
+    """
+    ss = StandardScaler(copy = is_copy, with_mean = with_mean, with_std = with_std)
+    transformed_data = ss.fit_transform(data)
 
-	return transformed_data
+    return transformed_data
 
 
-def normal(data):
-	"""
-	正规化：将特征变量的每个值正规化到某个区间，比如:[0, 1]
-	:param data:
-	:return:
-	"""
-	pass
+def min_max(data):
+    """
+    min-max 缩放
+    """
+    mms = MinMaxScaler()
+    transformed_data = mms.fit_transform()
+
+    return transformed_data
 
 
 def normalizer(data, norm, axis, is_copy = True, return_norm = False):
-	"""
-	正则化:将每个样本或特征正则化为L1, L2范数
-	:return:
-	"""
-	transformed_data = normalize(X = data,
-								 norm = norm,
-								 axis = axis,
-								 copy = is_copy,
-								 return_norm = return_norm)
+    """
+    正则化:将每个样本或特征正则化为L1, L2范数
+    """
+    transformed_data = normalize(X = data,
+                                 norm = norm,
+                                 axis = axis,
+                                 copy = is_copy,
+                                 return_norm = return_norm)
 
-	return transformed_data
+    return transformed_data
+
+
+def robust(data):
+    """
+    稳健缩放
+    """
+    rs = RobustScaler()
+    transformed_data = RobustScaler(data)
+
+    return transformed_data
+
+
+
+
+def log_transform(feat):
+    feat_trans = np.log1p(feat)
+
+    return feat_trans
+
+def Box_Cox(feat):
+    bc = Powertransformer(method = "box-cox", standardize = False)
+    feat_trans = bc.fit_transform(feat)
+
+    return feat_trans
+
+def yeo_johnson():
+    yj = Powertransformer(method = "yeo-johnson", standardize = False)
+    feat_trans = yj.fit_transform(feat)
+    
+    return feat_trans
+
+
+
+
+
+# ================================================
+# 类别性特征重编码
+# ================================================
+
+def oneHotEncoding(data, limit_value = 10):
+    """
+    One-Hot Encoding: pandas get_dummies
+    """
+    feature_cnt = data.shape[1]
+    class_index = []
+    class_df = pd.DataFrame()
+    normal_index = []
+    for i in range(feature_cnt):
+        if len(pd.DataFrame(data.iloc[:, i]).drop_duplicates()) < limit_value:
+            class_index.append(i)
+            class_df = pd.concat([class_df, pd.get_dummies(data.iloc[:, i], prefix = data.columns[i])], axis = 1)
+        else:
+            normal_index.append(i)
+    data_update = pd.concat([data.iloc[:, normal_index], class_df], axis = 1)
+    return data_update
+
+
+def order_encoder(cate_feats):
+    enc = OrdinalEncoder()
+    encoded_feats = enc.fit_transform(cate_feats)
+
+    return encoded_feats
+
+
+def one_hot_encoder(cate_feats):
+    """
+    One-Hot Encoding: sklearn.preprocessing.OneHotEncoder
+    """
+    enc = OneHotEncoder()
+    encoded_feats = enc.fit_transform(cate_feats)
+
+    return encoded_feats
+
+
+
+# ================================================
+# 数值型变量分箱离散化
+# ================================================
+def binarization(data, threshold = 0.0, is_copy = True):
+    """
+    二值化
+    """
+    bined = Binarizer(threshold = threshold, copy = is_copy)
+    transformed_data = bined.fit_transform(data)
+
+    return transformed_data
+
+
+def k_bins(data, n_bins, encoder = "ordinal", strategy = "quantile"):
+    """
+    分箱离散化
+    * encode:
+        - "ordinal"
+        - "onehot"
+        - "onehot-dense"
+    * strategy:
+        - "uniform"
+        - "quantile"
+        - "kmeans"
+    """
+    est = preprocessing.KBinsDiscretizer(n_bins = n_bins, encoder = encoder, strategy = strategy)
+    transformed_data = est.fit_transform(data)
+
+    return transformed_data
+
+
+# ================================================
+# 数值型变量分箱离散化
+# ================================================
+
+
 
 def feature_hist(feat):
-	mpl.rcParams['figure.figsize'] = (12.0, 6.0)
-	prices = pd.DataFrame({
-		'%s' % feat: feat,
-		'log(1 + %s)' % feat: log_trans_norm(feat)
-	})
-	prices.hist()
+    mpl.rcParams['figure.figsize'] = (12.0, 6.0)
+    prices = pd.DataFrame({
+        '%s' % feat: feat,
+        'log(1 + %s)' % feat: log_trans_norm(feat)
+    })
+    prices.hist()
 
 
 def normality_transform(feature):
-	"""
-	# Map data from any distribution to as close to Gaussian distribution as possible
-	# in order to stabilize variance and minimize skewness:
-	# 	- log(1 + x) transform
-	# 	- Yeo-Johnson transform
-	# 	- Box-Cox transform
-	# 	- Quantile transform
-	:param feature:
-	:return:
-	"""
-	pass
+    """
+    # Map data from any distribution to as close to Gaussian distribution as possible
+    # in order to stabilize variance and minimize skewness:
+    #   - log(1 + x) transform
+    #   - Yeo-Johnson transform
+    #   - Box-Cox transform
+    #   - Quantile transform
+    """
+    pass
 
 
 
-def log_trans_norm(feat):
-	feat_trans = np.log1p(feat)
 
-	return feat_trans
-
-
-def box_cox(feat):
-	bc = PowerTransformer(method="box-cox", standardize=False)
-	feat_trans = bc.fit_transfrom(feat)
-
-	return feat_trans
-
-def yeo_johnson():
-	yj = PowerTransformer(method = "yeo-johnson", standardize = False)
-	feat_trans = yj.fit_transfrom(feat)
-	
-	return feat_trans
 
 
 def quantileNorm(feat):
-	qt = QuantileTransformer(output_distribution = "normal", random_state = 0)
-	feat_trans = qt.fit_transform(feat)
+    qt = Quantiletransformer(output_distribution = "normal", random_state = 0)
+    feat_trans = qt.fit_transform(feat)
 
-	return feat_trans
+    return feat_trans
 
 def quantileUniform(feat, feat_test = None):
-	qu = QuantileTransformer(random_state = 0)
-	feat_trans = qu.fit_transform(feat)
-	feat_trans_test = qu.transform(feat_test)
+    qu = Quantiletransformer(random_state = 0)
+    feat_trans = qu.fit_transform(feat)
+    feat_trans_test = qu.transform(feat_test)
 
-	return feat, feat_trans_test
+    return feat, feat_trans_test
 
 def feature_dtype(data):
-	with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-		print(data.dtypes)
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+        print(data.dtypes)
 
 def numeric_categorical_features(data, limit_value = 0):
-	columns = data.columns
+    columns = data.columns
 
-	num_feature_idx = []
-	cate_feature_idx = []
-	for i in columns:
-		if (data[i].dtypes != "object") & (len(set(data[i])) >= limit_value):
-			num_feature_idx.append(i)
-		else:
-			cate_feature_idx.append(i)
+    num_feature_idx = []
+    cate_feature_idx = []
+    for i in columns:
+        if (data[i].dtypes != "object") & (len(set(data[i])) >= limit_value):
+            num_feature_idx.append(i)
+        else:
+            cate_feature_idx.append(i)
 
-	num_feat_index = data[num_feature_idx].columns
-	num_feat = data[num_feature_idx]
-	cate_feat_index = data[cate_feature_idx].columns
-	cate_feat = data[cate_feature_idx]
+    num_feat_index = data[num_feature_idx].columns
+    num_feat = data[num_feature_idx]
+    cate_feat_index = data[cate_feature_idx].columns
+    cate_feat = data[cate_feature_idx]
 
-	return num_feat, num_feat_index, cate_feat, cate_feat_index
+    return num_feat, num_feat_index, cate_feat, cate_feat_index
 
 
 def skewed_features(data, num_feat_idx, limit_value = 0.75):
-	skewed_feat = data[num_feat_idx].apply(lambda x: skew(x.dropna()))
-	skewed_feat = skewed_feat[np.abs(skewed_feat) > limit_value]
-	skewed_feat_index = skewed_feat.index
+    skewed_feat = data[num_feat_idx].apply(lambda x: skew(x.dropna()))
+    skewed_feat = skewed_feat[np.abs(skewed_feat) > limit_value]
+    skewed_feat_index = skewed_feat.index
 
-	return skewed_feat, skewed_feat_index
+    return skewed_feat, skewed_feat_index
 
-def targetTransfromer():
-	trans = QuantileTransformer(output_distribution = "normal")
-	return trans
+
+
+
+
+def targettransformer():
+    trans = Quantiletransformer(output_distribution = "normal")
+    return trans
+
 
 def binarize_label(y, classes_list):
-	y = label_binarize(y, classes = classes_list)
+    y = label_binarize(y, classes = classes_list)
 
-	return y
-
-
+    return y
 
 
+
+# ################################################
 
 def main():
-	X = np.array([["male"], ['female'], ['male']])
-	X = pd.DataFrame(X)
-	encoded_X = one_hot_encoder(X)
-	print(encoded_X)
-
-	data = oneHotEncoding(X)
-	print(data)
+    pass
 
 if __name__ == "__main__":
-	main()
+    main()
